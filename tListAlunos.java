61e2e8385382f1a957f4f33542170a80bfdc1328
Herbert Andrade Nascimento <>
2021-05-17 12:31:49 +0000

    separando os métodos

--- tListAlunos.java
+++ tListAlunos.java
@@ -25,121 +25,151 @@ public class tListAlunos {
         this.isOrdenada = isOrdenada;
     }
 
-    private int buscar(String key) {
-        int i = 0;
-        if (this.isOrdenada) {
-            int min = 0, max = this.tam;
-            while (min != max) {
-                i = (max + min) / 2;
-                int com = this.listAluno[i].getMatricula().compareTo(key);
-                if (com < 0) {
-                    min = ++i;
+    private int buscaOrdenada(String key) {
+        int min = 0, max = this.tam, i = 0;
+        while (min != max) {
+            i = (max + min) / 2;
+            int com = this.listAluno[i].getMatricula().compareTo(key);
+            if (com < 0) {
+                min = ++i;
+            } else {
+                if (com > 0) {
+                    max = i;
                 } else {
-                    if (com > 0) {
-                        max = i;
-                    } else {
-                        if (i < this.tam) {
-                            this.achou = true;
-                        }
-                        return i;
+                    if (i < this.tam) {
+                        this.achou = true;
                     }
+                    return i;
                 }
             }
-            return i;
+        }
+        return i;
+    }
+
+    private int buscaNaoOrdenada(String key) {
+        int n = this.tam, i = 0;
+        this.listAluno[n] = new Aluno(key, null, null);
+        while (this.listAluno[i].getMatricula().compareTo(key) != 0) {
+            i++;
+        }
+        this.listAluno[n] = new Aluno();
+        return i;
+    }
+
+    public int buscar(String matricula) {
+        int index;
+        if (this.isOrdenada) {
+            index = buscaOrdenada(matricula);
         } else {
-            int n = this.tam;
-            this.listAluno[n] = new Aluno(key, null, null);
-            while (this.listAluno[i].getMatricula().compareTo(key) != 0) {
-                i++;
+            index = buscaNaoOrdenada(matricula);
+        }
+        return index;
+    }
+
+    private boolean inclusaoOrdenada(Aluno aluno) {
+        int n = this.tam;
+        int i = buscar(aluno.getMatricula());
+        if (i == n && n < this.cap) {
+            this.listAluno[n] = new Aluno(aluno.getMatricula(), aluno.getNome(), aluno.getEmail());
+            this.tam++;
+            return true;
+        } else if (!this.achou) {																								// se não achei na lista
+            Aluno troca = new Aluno();																							// variavel de troca
+            for (int j = i; j < n; j++) {
+                troca.setMatricula(this.listAluno[j].getMatricula());
+                troca.setNome(this.listAluno[j].getNome());
+                troca.setEmail(this.listAluno[j].getEmail());
+
+                this.listAluno[j].setMatricula(aluno.getMatricula());
+                this.listAluno[j].setNome(aluno.getNome());
+                this.listAluno[j].setEmail(aluno.getEmail());
+
+                aluno.setMatricula(troca.getMatricula());
+                aluno.setNome(troca.getNome());
+                aluno.setEmail(troca.getEmail());
             }
-            this.listAluno[n] = new Aluno();
-            return i;
+            this.listAluno[n] = new Aluno(aluno.getMatricula(), aluno.getNome(), aluno.getEmail());
+            this.tam++;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean inclusaoNaoOrdenada(Aluno aluno) {
+        int n = this.tam;
+        if (n <= this.cap) {
+            if (buscar(aluno.getMatricula()) == n) {
+                this.listAluno[n] = new Aluno(aluno.getMatricula(), aluno.getNome(), aluno.getEmail());
+                this.tam++;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
         }
     }
 
     public boolean incluir(Aluno aluno) {
-        int n = this.tam;
+        boolean ok;
         if (this.isOrdenada) {
+            ok = inclusaoOrdenada(aluno);
+        } else {
+            ok = inclusaoNaoOrdenada(aluno);
+        }
+        return ok;
+    }
+
+    private boolean remocaoOrdenada(Aluno aluno) {
+        int n = this.tam;
+        if (n > 0) {
             int i = buscar(aluno.getMatricula());
-            if (i == n && n < this.cap) {
-                this.listAluno[n] = new Aluno(aluno.getMatricula(), aluno.getNome(), aluno.getEmail());
-                this.tam++;
-                return true;
-            } else if (!this.achou) {																								// se não achei na lista
-                Aluno troca = new Aluno();																							// variavel de troca
+            if (this.achou) {
+                this.listAluno[this.tam] = new Aluno();
                 for (int j = i; j < n; j++) {
-                    troca.setMatricula(this.listAluno[j].getMatricula());
-                    troca.setNome(this.listAluno[j].getNome());
-                    troca.setEmail(this.listAluno[j].getEmail());
-
-                    this.listAluno[j].setMatricula(aluno.getMatricula());
-                    this.listAluno[j].setNome(aluno.getNome());
-                    this.listAluno[j].setEmail(aluno.getEmail());
-
-                    aluno.setMatricula(troca.getMatricula());
-                    aluno.setNome(troca.getNome());
-                    aluno.setEmail(troca.getEmail());
+                    this.listAluno[j].setMatricula(this.listAluno[j + 1].getMatricula());
+                    this.listAluno[j].setNome(this.listAluno[j + 1].getNome());
+                    this.listAluno[j].setEmail(this.listAluno[j + 1].getEmail());
                 }
-                this.listAluno[n] = new Aluno(aluno.getMatricula(), aluno.getNome(), aluno.getEmail());
-                this.tam++;
+                this.tam--;
                 return true;
             } else {
                 return false;
             }
         } else {
-            if (n <= this.cap) {
-                if (buscar(aluno.getMatricula()) == n) {
-                    this.listAluno[n] = new Aluno(aluno.getMatricula(), aluno.getNome(), aluno.getEmail());
-                    this.tam++;
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
+            return false;
         }
+    }
 
+    private boolean remocaoNaoOrdenada(Aluno aluno) {
+        int n = this.tam;
+        if (n > 0) {																										// se a lista não estiver vazia
+            int i = buscar(aluno.getMatricula());
+            if (i < n) {																									// se achei na lista
+                for (int j = i; j < n; j++) {																// removo o elemento de i
+                    this.listAluno[j].setMatricula(this.listAluno[j + 1].getMatricula());
+                    this.listAluno[j].setNome(this.listAluno[j + 1].getNome());
+                    this.listAluno[j].setEmail(this.listAluno[j + 1].getEmail());
+                }
+                this.tam--;																							// e decrementa o tamanho
+                return true;																							// consegui remover
+            } else {
+                return false; 																				// não cosnegui pq não achei		
+            }
+        } else {
+            return false;
+        }
     }
 
     public boolean remover(Aluno aluno) {
-        int n = this.tam;
-        if (isOrdenada) {
-            if (n > 0) {
-                int i = buscar(aluno.getMatricula());
-                if (this.achou) {
-                    this.listAluno[this.tam] = new Aluno();
-                    for (int j = i; j < n; j++) {
-                        this.listAluno[j].setMatricula(this.listAluno[j + 1].getMatricula());
-                        this.listAluno[j].setNome(this.listAluno[j + 1].getNome());
-                        this.listAluno[j].setEmail(this.listAluno[j + 1].getEmail());
-                    }
-                    this.tam--;
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
+        boolean ok;
+        if (this.isOrdenada) {
+            ok = remocaoOrdenada(aluno);
         } else {
-            if (n > 0) {																										// se a lista não estiver vazia
-                int i = buscar(aluno.getMatricula());
-                if (i < n) {																									// se achei na lista
-                    for (int j = i; j < n; j++) {																// removo o elemento de i
-                        this.listAluno[j].setMatricula(this.listAluno[j + 1].getMatricula());
-                        this.listAluno[j].setNome(this.listAluno[j + 1].getNome());
-                        this.listAluno[j].setEmail(this.listAluno[j + 1].getEmail());
-                    }
-                    this.tam--;																							// e decrementa o tamanho
-                    return true;																							// consegui remover
-                } else {
-                    return false; 																				// não cosnegui pq não achei		
-                }
-            } else {
-                return false;
-            }
+            ok = remocaoNaoOrdenada(aluno);
         }
+        return ok;
     }
 
     public void printList() {
@@ -149,11 +179,11 @@ public class tListAlunos {
     }
 
     public Aluno getListAluno(int n) {
-        if(n<0 || n>=this.tam){
+        if (n < 0 || n >= this.tam) {
             return null;
-        }else{
+        } else {
             return listAluno[n];
-        }       
+        }
     }
 
     public Aluno getAluno(String matricula) {
@@ -174,3 +204,4 @@ public class tListAlunos {
     }
 
 }
+
